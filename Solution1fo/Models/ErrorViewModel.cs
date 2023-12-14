fbc06b193865ea077d3ec4f46c76252357748e42
sckald <damienchereault@gmail.com>
2023-12-14 17:12:04 +0100

    init git

---
+++ Solution1fo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Solution1fo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
