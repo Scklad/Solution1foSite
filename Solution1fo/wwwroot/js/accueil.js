34b7395dad99d106c094fffaad15ecdc59efd9fe
sckald <damienchereault@gmail.com>
2024-01-26 15:19:17 +0100

    Update home page new design

--- Solution1fo/wwwroot/js/accueil.js
+++ Solution1fo/wwwroot/js/accueil.js
@@ -13,3 +13,20 @@ window.onscroll = function (e) {
         header.classList.add("bg-opaque-fixed");
     }
 }
+
+$("#navbar_button").click(function (e) {
+    var header = document.getElementById("navbar");
+    alert("test");
+    if (window.pageYOffset == 0 && header.classList.contains("bg-opaque")) {
+        header.classList.remove("bg-opaque");
+        header.classList.add("bg-opaque-fixed");
+    }
+    else if (window.pageYOffset == 0 && header.classList.contains("bg-opaque-fixed")) {
+        header.classList.add("bg-opaque");
+        header.classList.remove("bg-opaque-fixed");
+    }
+    else {
+        header.classList.remove("bg-opaque");
+        header.classList.add("bg-opaque-fixed");
+    }
+});
