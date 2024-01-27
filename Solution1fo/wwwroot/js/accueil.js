1c76638bcf04fd9dbfe8100d6add2030c7de8051
sckald <damienchereault@gmail.com>
2024-01-27 16:30:37 +0100

    correct home page and h2

--- Solution1fo/wwwroot/js/accueil.js
+++ Solution1fo/wwwroot/js/accueil.js
@@ -4,7 +4,8 @@
 // Write your JavaScript code.
 window.onscroll = function (e) {
     var header = document.getElementById("navbar");
-    if (window.pageYOffset == 0) {
+    var navbar_deployed = document.getElementById("navbar_deployed");
+    if (window.pageYOffset == 0 && !(navbar_deployed.classList.contains("show"))) {
         header.classList.add("bg-opaque");
         header.classList.remove("bg-opaque-fixed");
     }
@@ -14,19 +15,37 @@ window.onscroll = function (e) {
     }
 }
 
-$("#navbar_button").click(function (e) {
+var element = document.getElementById('navbar_deployed');
+
+// Créez une instance de MutationObserver avec une fonction de rappel
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        // Vérifiez si la classe a changé
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+            // La classe a changé, effectuez votre action ici
+            console.log('La classe a changé : ', element.className);
+
+            // Appelez votre fonction ou effectuez votre action ici
+            votreFonction();
+        }
+    });
+});
+
+// Configurez l'observateur pour surveiller les modifications de classe
+var config = { attributes: true, attributeFilter: ['class'] };
+observer.observe(element, config);
+
+// Fonction à exécuter lorsque la classe change
+function votreFonction() {
+    // Votre code ici
     var header = document.getElementById("navbar");
-    alert("test");
-    if (window.pageYOffset == 0 && header.classList.contains("bg-opaque")) {
+    var navbar_deployed = document.getElementById("navbar_deployed");
+    if (navbar_deployed.classList.contains("show")) {
         header.classList.remove("bg-opaque");
         header.classList.add("bg-opaque-fixed");
     }
-    else if (window.pageYOffset == 0 && header.classList.contains("bg-opaque-fixed")) {
+    else if (window.pageYOffset == 0) {
         header.classList.add("bg-opaque");
         header.classList.remove("bg-opaque-fixed");
     }
-    else {
-        header.classList.remove("bg-opaque");
-        header.classList.add("bg-opaque-fixed");
-    }
-});
+}
